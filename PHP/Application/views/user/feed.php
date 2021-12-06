c3414e04337c35bdc6cf8fba4e7e51148ade0c57
Vanessa <vanessarodriguescardoso8@gmail.com>
2021-12-06 12:07:23 -0300

    Arrumando componente de sugestões

--- PHP/Application/views/user/feed.php
+++ PHP/Application/views/user/feed.php
@@ -57,95 +57,108 @@
 </header>
 
 <div class="general">
-    <div class="container d-flex justify-content-between">
-        <aside class="nav-aside">
-            <div class="profile d-flex flex-column mt-5">
-                <div class="container px-4 py-4">
-                    <div class="information-profile d-flex flex-column align-items-center pt-4">
-                        <img src="/assets/img/foto-luna.png" alt="Foto Luna">
-                        <div class="name-profile d-flex flex-column align-items-center pt-4 pb-4 gap-2">
-                            <h3>Luna Maria</h3>
-                            <h4>@LunaM</h4>
+    <div class="container d-flex">
+        <div class="">
+            <aside class="nav-aside">
+                <div class="profile d-flex flex-column mt-5">
+                    <div class="container px-4 py-4">
+                        <div class="information-profile d-flex flex-column align-items-center pt-4">
+                            <img src="/assets/img/foto-luna.png" alt="Foto Luna">
+                            <div class="name-profile d-flex flex-column align-items-center pt-4 pb-4 gap-2">
+                                <h3>Luna Maria</h3>
+                                <h4>@LunaM</h4>
+                            </div>
                         </div>
-                    </div>
-        
-                    <div class="score-profile">
-                        <div class="score-post d-flex justify-content-between pt-4 pb-3">
-                            <h4>Total de postagens</h4>
-                            <p>256</p>
+
+                        <div class="score-profile">
+                            <div class="score-post d-flex justify-content-between pt-4 pb-3">
+                                <h4>Total de postagens</h4>
+                                <p>256</p>
+                            </div>
+
+                            <div class="score-avaliation d-flex justify-content-between pb-4">
+                                <h4>Avaliações Realizadas</h4>
+                                <p>301</p>
+                            </div>
                         </div>
-        
-                        <div class="score-avaliation d-flex justify-content-between pb-4">
-                            <h4>Avaliações Realizadas</h4>
-                            <p>301</p>
+
+                        <!-- COLOCAR REDIRECIONAMENTO DOS LINKS e ALINHAR IMAGEM COM ÍCONE -->
+                        <div class="navegation-profile d-flex flex-column gap-4 pb-4">
+                            <a class="d-flex align-items-center" href=""><img class="pe-2" src="/assets/img/icon-home.svg" alt="Ícone home"> Página inicial</a>
+                            <a class="d-flex align-items-center" href=""><img class="pe-2" src="/assets/img/icon-profile.svg" alt="Ícone perfil"> Perfil</a>
+                            <a class="d-flex align-items-center" href=""><img class="pe-2" src="/assets/img/icon-plus.svg" alt="Ícone mais"> Nova publicação</a>
+                            <a class="d-flex align-items-center" href=""><img class="pe-2" src="/assets/img/icon-config.svg" alt="Ícone configurações">Configurações</a>
                         </div>
-                    </div>
-        
-                    <!-- COLOCAR REDIRECIONAMENTO DOS LINKS e ALINHAR IMAGEM COM ÍCONE -->
-                    <div class="navegation-profile d-flex flex-column gap-4 pb-4">
-                        <a class="d-flex align-items-center" href=""><img class="pe-2" src="/assets/img/icon-home.svg" alt="Ícone home"> Página inicial</a>
-                        <a class="d-flex align-items-center" href=""><img class="pe-2" src="/assets/img/icon-profile.svg" alt="Ícone perfil"> Perfil</a>
-                        <a class="d-flex align-items-center" href=""><img class="pe-2" src="/assets/img/icon-plus.svg" alt="Ícone mais"> Nova publicação</a>
-                        <a class="d-flex align-items-center" href=""><img class="pe-2" src="/assets/img/icon-config.svg" alt="Ícone configurações">Configurações</a>
                     </div>
                 </div>
-            </div>
-        
-            <div class="chatbot mt-5">
-                <div class="container px-4 py-4">
-                    <h3>Aqui vai o chatbot, escrevi isso só para div aparecer :</h3>
+
+                <div class="chatbot mt-5">
+                    <div class="container px-4 py-4">
+                        <h3>Aqui vai o chatbot, escrevi isso só para div aparecer :</h3>
+                    </div>
                 </div>
-            </div>
-        
-            <div class="suggestions mt-5">
-                <div class="container px-4 py-4">
-        
-                    <!-- COLOCAR LINKS NOS @'S -->
-            
-                    <div class="suggestion">
-                        <div class="accordion" id="accordionPanelsStayOpenExample">
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                                        Sugestões para você
-                                    </button>
-                                </h2>
-                                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                                    <div class="accordion-body">
-                                        <div class="suggestion-sv d-flex justify-content-around pt-4 pb-4">
-                                            <img src="/assets/img/foto-suzana-v.svg" alt="Foto Suzana Vieira">
-                                            <div class="name-suggestion">
-                                                <h4>Suzana Vieira</h4>
-                                                <p>@suzanavieira</p>
+
+                <div class="suggestions mt-5">
+                    <div class="container px-4 py-4">
+
+                        <!-- COLOCAR LINKS NOS @'S -->
+
+                        <div class="suggestion">
+                            <div class="accordion" id="accordionPanelsStayOpenExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                                            Sugestões para você
+                                        </button>
+                                    </h2>
+                                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show position-relative" aria-labelledby="panelsStayOpen-headingOne">
+                                        <div class="accordion-body">
+                                            <div class="position-absolute">
+
+                                                <div class="sugest d-flex mt-2 mb-2">
+                                                    <div class="sugest d-flex">
+                                                        <img src="/assets/img/foto-suzana-v.svg">
+                                                        <div class="nome">
+                                                            <a class="text-black links">Suzana Vieira</a>
+                                                            <a class="text-black links">@suzanavieira</a>
+                                                        </div>
+                                                    </div>
+                                                    <span class="links">Seguir</span>
+                                                </div>
+
+                                                <div class="sugest d-flex mt-2 mb-2">
+                                                    <div class="sugest d-flex">
+                                                        <img src="/assets/img/foto-jojo-t.svg">
+                                                        <div class="nome">
+                                                            <a class="text-black links">Jojo Toddynho</a>
+                                                            <a class="text-black links">@jojguerreira</a>
+                                                        </div>
+                                                    </div>
+                                                    <span class="links">Seguir</span>
+                                                </div>
+
+                                                <div class="sugest d-flex mt-2 mb-2">
+                                                    <div class="sugest d-flex">
+                                                        <img src="/assets/img/foto-anitta.svg">
+                                                        <div class="nome">
+                                                            <a class="text-black links">Anitta</a>
+                                                            <a class="text-black links">@anitta</a>
+                                                        </div>
+                                                    </div>
+                                                    <span class="links">Seguir</span>
+                                                </div>
+
+                                                <div class="sugest d-flex mt-2 mb-2">
+                                                    <div class="sugest d-flex">
+                                                        <img src="/assets/img/foto-suzane-r.svg">
+                                                        <div class="nome">
+                                                            <a class="text-black links">Su</a>
+                                                            <a class="text-black links">@surichthofen</a>
+                                                        </div>
+                                                    </div>
+                                                    <span class="links">Seguir</span>
+                                                </div>
                                             </div>
-                                            <p class="follow">Seguir</p>
-                                        </div>
-        
-                                        <div class="suggestion-jt d-flex justify-content-around pt-4 pb-4">
-                                            <img src="/assets/img/foto-jojo-t.svg" alt="Foto Jojo Toddynho">
-                                            <div class="name-suggestion">
-                                                <h4>Jojo Toddynho</h4>
-                                                <p>@jojoguerreira</p>
-                                            </div>
-                                            <p class="follow">Seguir</p>
-                                        </div>
-        
-                                        <div class="suggestion-ant d-flex justify-content-around pt-4 pb-4">
-                                            <img src="/assets/img/foto-anitta.svg" alt="Foto Anitta">
-                                            <div class="name-suggestion">
-                                                <h4>Anitta Machado</h4>
-                                                <p>@anitta</p>
-                                            </div>
-                                            <p class="follow">Seguir</p>
-                                        </div>
-        
-                                        <div class="suggestion-sr d-flex justify-content-around pt-4 pb-4">
-                                            <img src="/assets/img/foto-suzane-r.svg" alt="Foto Suzane Richthofen">
-                                            <div class="name-suggestion">
-                                                <h4>Su Richthofen</h4>
-                                                <p>@su_richthofen</p>
-                                            </div>
-                                            <p class="follow">Seguir</p>
                                         </div>
                                     </div>
                                 </div>
@@ -153,8 +166,9 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </aside>
+            </aside>
+        </div>
+
         <div class="mx-5 feed d-inline-flex">
             <div class="feed-posts position-relative w-post m-3 mt-0">
                 <div class="dates d-flex align-items-center gap-4 position-relative">
@@ -164,7 +178,7 @@
                         <p>08.12.2019</p>
                     </div>
                 </div>
-                
+
                 <div class="background position-relative w-fit-content">
                     <div>
                         <img class="d-block shadow-sm w-100" src="/assets/img/post-1.svg" alt="Exposição de arte">
@@ -179,7 +193,7 @@
                                 <img src="/assets/img/like-disabled.svg" alt="Botão de like">
                             </button>
                         </div>
-        
+
                         <div class="coment">
                             <button id="btn-coment">
                                 <img src="/assets/img/comment.svg" alt="Botão de comentar">
@@ -196,7 +210,7 @@
                         <p>08.12.2019</p>
                     </div>
                 </div>
-                
+
                 <div class="background position-relative w-fit-content">
                     <div>
                         <img class="d-block shadow-sm w-100" src="/assets/img/post-1.svg" alt="Exposição de arte">
@@ -211,7 +225,7 @@
                                 <img src="/assets/img/like-disabled.svg" alt="Botão de like">
                             </button>
                         </div>
-        
+
                         <div class="coment">
                             <button id="btn-coment">
                                 <img src="/assets/img/comment.svg" alt="Botão de comentar">
@@ -220,8 +234,9 @@
                     </div>
                 </div>
             </div>
-        
+
             <!-- COLOCAR MINI FOOTER AQUI -->
         </div>
-    
     </div>
+
+</div>
